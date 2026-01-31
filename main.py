5577365d57abc42d8b7122096df98e78258dc133
SasBorealis <jacobhellinger227@gmail.com>
2026-01-31 13:04:32 -0600

    Player object finished

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
-clock = pygame.time.Clock()
-dt = 0
+from player import Player
+updatable = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
 
 def main():
     pygame.init()
-    print("Starting Asteroids with pygame version: 2.6.1")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
 
+        updatable.update(dt)
+
+        screen.fill("black")
+        for drawables in drawable:
+            drawables.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
+
